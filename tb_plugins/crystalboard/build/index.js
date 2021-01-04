a27a77af95ed83b1bb49ce40a72c89b7c12d27b1
Yuxing Fei <36567193+idocx@users.noreply.github.com>
2021-01-04 16:52:20 +0800

    ready to use

---
+++ tb_plugins/crystalboard/build/index.js
@@ -0,0 +1,15 @@
+export function render() {
+  
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.body.appendChild(root);
+  const s0 = document.createElement('script');
+  s0.src = 'static/js/preLoad.js';
+  document.body.appendChild(s0);
+  const s1 = document.createElement('script');
+  s1.src = "static/js/2.98cdc7c8.chunk.js";
+  document.body.appendChild(s1);
+  const s2 = document.createElement('script');
+  s2.src = "static/js/main.1004d281.chunk.js";
+  document.body.appendChild(s2);
+}
